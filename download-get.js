c0adfdd32cf4a436aea943a5fa66ee73da2cabb6
mofengfly <wxl920@126.com>
2013-11-16 19:27:54 +0800

    Update download-get.js

--- download-get.js
+++ download-get.js
@@ -37,3 +37,40 @@ http.get(options, function(res) {
         });
     });
 };
+// Function to download file using curl
+var download_file_curl = function(file_url) {
+
+    // extract the file name
+    var file_name = url.parse(file_url).pathname.split('/').pop();
+    // create an instance of writable stream
+    var file = fs.createWriteStream(DOWNLOAD_DIR + file_name);
+    // execute curl using child_process' spawn function
+    var curl = spawn('curl', [file_url]);
+    // add a 'data' event listener for the spawn instance
+    curl.stdout.on('data', function(data) { file.write(data); });
+    // add an 'end' event listener to close the writeable stream
+    curl.stdout.on('end', function(data) {
+        file.end();
+        console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+    });
+    // when the spawn child process exits, check if there were any errors and close the writeable stream
+    curl.on('exit', function(code) {
+        if (code != 0) {
+            console.log('Failed: ' + code);
+        }
+    });
+};
+// Function to download file using wget
+var download_file_wget = function(file_url) {
+
+    // extract the file name
+    var file_name = url.parse(file_url).pathname.split('/').pop();
+    // compose the wget command
+    var wget = 'wget -P ' + DOWNLOAD_DIR + ' ' + file_url;
+    // excute wget using child_process' exec function
+
+    var child = exec(wget, function(err, stdout, stderr) {
+        if (err) throw err;
+        else console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+    });
+};
